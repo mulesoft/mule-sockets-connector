3a0efab052feb5f67a132191687aa5e011fd1b07
Alejandro Iannucci <4360246+aiannucci@users.noreply.github.com>
2019-04-01 16:06:18 -0300

    MULE-16632: Add missing equals and hashCode to Sockets connector operations

--- src/main/java/org/mule/extension/socket/api/SocketConnectionSettings.java
+++ src/main/java/org/mule/extension/socket/api/SocketConnectionSettings.java
@@ -13,6 +13,7 @@ import org.mule.runtime.extension.api.annotation.param.Parameter;
 import org.mule.runtime.extension.api.annotation.param.display.Placement;
 
 import java.net.InetSocketAddress;
+import java.util.Objects;
 
 /**
  * Groups host and port fields.
@@ -80,5 +81,23 @@ public class SocketConnectionSettings {
 
   private boolean isEmpty() {
     return isBlank(host) && port == null;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    SocketConnectionSettings that = (SocketConnectionSettings) o;
+    return Objects.equals(port, that.port) &&
+        Objects.equals(host, that.host);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(port, host);
   }
 }
